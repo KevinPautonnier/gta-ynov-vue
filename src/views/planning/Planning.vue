09f59a68eb431f0040a5235d508b34073103a6bf
KevinPautonnier <kevin.pautonnier@ynov.com>
2018-11-27 22:52:04 +0100

    add count hours and fix some errors

--- src/views/planning/Planning.vue
+++ src/views/planning/Planning.vue
@@ -1,8 +1,9 @@
 
 <template>
   <div>
-    <full-calendar :events="events" :header="header" @event-selected="eventSelected" @event-drop="eventDragStop" @event-resize="eventResizeStop"></full-calendar>
+    <div>Nombre d'heure éffectuées : {{hours}}</div>
     <button v-on:click="saveData" v-if="dataModified">Enregistrer les modifications</button>
+    <full-calendar :events="events" :header="header" @event-selected="eventSelected" @event-drop="eventDragStop" @event-resize="eventResizeStop"></full-calendar>
   </div>
 </template>
 
@@ -20,7 +21,8 @@
           center: 'title',
         },
         events: JSON.parse(sessionStorage.getItem("planningData")),
-        dataModified : _dataModified
+        dataModified : _dataModified,
+        hours: 0
       }
     },
 
@@ -37,6 +39,7 @@
             this.events.forEach(function(event){
               if(event.title == calEvent.title){
                 event.start = moment(newDate.setHours(startHour))
+                event.end = moment(newDate.setHours(endHour))
               }
             })
             this.dataModified = true
@@ -46,7 +49,7 @@
       eventDragStop: function(calEvent){
         console.log('drop')
         
-        if(sessionStorage.getItem('role') == 'Salaried' || sessionStorage.getItem('role') == 'Team Manager'){
+        if(sessionStorage.getItem('role') == 'Salaried' || sessionStorage.getItem('role') == 'Human Resourses Director'){
           alert('Vous n\'avez pas le droit de modifier votre planning!');
           this.events = JSON.parse(sessionStorage.getItem("planningData"));
         }
@@ -64,7 +67,7 @@
       eventResizeStop: function(calEvent){
         console.log('resize')
         
-        if(sessionStorage.getItem('role') == 'Salaried' || sessionStorage.getItem('role') == 'Team Manager'){
+        if(sessionStorage.getItem('role') == 'Salaried' || sessionStorage.getItem('role') == 'Human Resourses Director'){
           alert('Vous n\'avez pas le droit de modifier votre planning!');
           this.events = JSON.parse(sessionStorage.getItem("planningData"));
         }
@@ -82,7 +85,27 @@
       saveData: function(){
         console.log('dataSave')
         sessionStorage.setItem("planningData", JSON.stringify(this.events))
+        this.hourCount();
+      },
+      hourCount: function(){
+        var data = JSON.parse(sessionStorage.getItem("planningData"))
+        this.hours = 0
+        var _this = this
+        var now = new Date();
+        data.forEach(function(day){
+          if(moment(now).startOf('day') <= moment(day.start).startOf('day')){
+            return
+          }
+          else{
+            var start = moment(day.start)
+            var end = moment(day.end)
+            _this.hours += moment.duration(end.diff(start)).asHours()
+          }
+        })
       }
+    },
+    created: function(){
+      this.hourCount();
     }
   }
 </script>
